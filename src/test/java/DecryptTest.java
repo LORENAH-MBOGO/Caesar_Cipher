defcc2117e85e127c9a26f4a0aa8880378846e97
Lorenah <lorayne911@gmail.com>
2020-11-30 17:14:03 +0300

    added README file

--- src/test/java/DecryptTest.java
+++ src/test/java/DecryptTest.java
@@ -1,9 +1,15 @@
 import junit.framework.TestCase;
 import org.junit.Test;
 
+import java.util.Scanner;
+
 public class DecryptTest extends TestCase {
     @Test
-    public void Decrypt_cipherText(){
-        Decrypt cipherText;
+    public void Decrypt(String plaintext, int shift) {
+        plaintext = "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG";
+        String ciphertext = "QEB NRFZH YOLTK CLU GRJMP LSBO QEB IXWV ALD";
+        shift = 1;
+        System.out.println("QEB NRFZH YOLTK CLU GRJMP LSBO QEB IXWV ALD");
     }
 }
+
