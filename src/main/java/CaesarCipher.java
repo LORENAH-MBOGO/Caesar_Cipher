13c549ed1472ab1524432e3b95b0ba98778a6a82
Lorenah <lorayne911@gmail.com>
2020-11-30 09:55:03 +0300

    ran first test

---
+++ src/main/java/CaesarCipher.java
@@ -0,0 +1,16 @@
+public class CaesarCipher {
+    StringBuilder cipher(String message, int offset) {
+        StringBuilder result = new StringBuilder();
+        for (char character : message.toCharArray()) {
+            if (character != ' ') {
+                int originalAlphabetPosition = character - 'a';
+                int newAlphabetPosition = (originalAlphabetPosition + offset) % 26;
+                char newCharacter = (char) ('a' + newAlphabetPosition);
+                result.append(newCharacter);
+            } else {
+                result.append(character);
+            }
+        }
+        return result;
+    }
+}
